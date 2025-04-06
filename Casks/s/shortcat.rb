eba664605070000be44af6537e20af508f60d977
chendo <chendo@users.noreply.github.com>
2025-04-06 15:29:45 +1000

    Shortcat v0.12.0

--- Casks/s/shortcat.rb
+++ Casks/s/shortcat.rb
@@ -1,9 +1,9 @@
 cask "shortcat" do
-  version "0.11.4"
-  sha256 "d2e840194e72f7ca0e2c094ced4bb8756c8af3945f351e5cf33dd4ecb1e2f20f"
+  version "0.12.0"
+  sha256 "944726bea5844e83eea1516c1f27857e343322ae0c8523503241bd416bdb1d00"
 
   url "https://files.shortcat.app/releases/v#{version}/Shortcat.zip"
-  name "Sproutcube Shortcat"
+  name "Shortcat"
   desc "App that enables mouse-free UI interaction"
   homepage "https://shortcat.app/"
 
@@ -13,7 +13,7 @@ cask "shortcat" do
   end
 
   auto_updates true
-  depends_on macos: ">= :big_sur"
+  depends_on macos: ">= :ventura"
 
   app "Shortcat.app"
 
